db4ab492c54a3eb68a47d194f501d02ced3280a9
Jing Liu <jintonic@physino.xyz>
2023-02-14 23:25:52 -0600

    added

---
+++ drawEinMuonPanels.C
@@ -0,0 +1,97 @@
+{
+	TChain tm100M("t");
+	tm100M.Add("mu-100MeV.root");
+	TH1F hm100M("hm100M",";Energy deposition of vertically "
+			"down-going muons in top panel [MeV];",60,0,30);
+	tm100M.Draw("et[1]/1000>>hm100M");
+
+	TChain tm1GeV("t");
+	tm1GeV.Add("mu-1GeV.root");
+	TH1F hm1GeV("hm1GeV","",60,0,30);
+	tm1GeV.Draw("et[1]/1000>>hm1GeV");
+
+	TChain tm4GeV("t");
+	tm4GeV.Add("mu-4GeV.root");
+	TH1F hm4GeV("hm4GeV","",60,0,30);
+	tm4GeV.Draw("et[1]/1000>>hm4GeV");
+
+	TChain tm20GeV("t");
+	tm20GeV.Add("mu-20GeV.root");
+	TH1F hm20GeV("hm20GeV","",60,0,30);
+	tm20GeV.Draw("et[1]/1000>>hm20GeV");
+
+	TChain tp100M("t");
+	tp100M.Add("mu+100MeV.root");
+	TH1F hp100M("hp100M","",60,0,30);
+	tp100M.Draw("et[1]/1000>>hp100M");
+
+	TChain tp1GeV("t");
+	tp1GeV.Add("mu+1GeV.root");
+	TH1F hp1GeV("hp1GeV","",60,0,30);
+	tp1GeV.Draw("et[1]/1000>>hp1GeV");
+
+	TChain tp4GeV("t");
+	tp4GeV.Add("mu+4GeV.root");
+	TH1F hp4GeV("hp4GeV","",60,0,30);
+	tp4GeV.Draw("et[1]/1000>>hp4GeV");
+
+	TChain tp20GeV("t");
+	tp20GeV.Add("mu+20GeV.root");
+	TH1F hp20GeV("hp20GeV","",60,0,30);
+	tp20GeV.Draw("et[1]/1000>>hp20GeV");
+
+	hm100M.SetStats(0);
+
+	hm100M.SetLineStyle(kDotted);
+	hm1GeV.SetLineStyle(kDotted);
+	hm4GeV.SetLineStyle(kDotted);
+	hm20GeV.SetLineStyle(kDotted);
+
+	hp1GeV.SetLineColor(kMagenta);
+	hp4GeV.SetLineColor(kBlue);
+	hp20GeV.SetLineColor(kGreen);
+
+	hm1GeV.SetLineColor(kMagenta);
+	hm4GeV.SetLineColor(kBlue);
+	hm20GeV.SetLineColor(kGreen);
+
+	hm100M.Draw();
+	hm1GeV.Draw("same");
+	hm4GeV.Draw("same");
+	hm20GeV.Draw("same");
+
+	hp100M.Draw("same");
+	hp1GeV.Draw("same");
+	hp4GeV.Draw("same");
+	hp20GeV.Draw("same");
+
+	TLegend leg(0.6,0.3,0.85,0.85);
+	leg.AddEntry(&hp100M, "#mu^{+}, 100 MeV", "l");
+	leg.AddEntry(&hp1GeV, "#mu^{+}, 1 GeV", "l");
+	leg.AddEntry(&hp4GeV, "#mu^{+}, 4 GeV", "l");
+	leg.AddEntry(&hp20GeV, "#mu^{+}, 20 GeV", "l");
+	leg.AddEntry(&hm100M, "#mu^{-}, 100 MeV", "l");
+	leg.AddEntry(&hm1GeV, "#mu^{-}, 1 GeV", "l");
+	leg.AddEntry(&hm4GeV, "#mu^{-}, 4 GeV", "l");
+	leg.AddEntry(&hm20GeV, "#mu^{-}, 20 GeV", "l");
+	leg.Draw();
+
+	TCanvas can; can.SetLogy();
+	TH1F hs20GeV("hs20GeV",";Energy deposition of vertically "
+			"down-going muons in panels [MeV];",60,0,30);
+	tp20GeV.Draw("et[2]/1000>>hs20GeV");
+	hs20GeV.SetStats(0);
+	hp20GeV.Draw("same");
+
+	TLegend legend(0.7,0.7,0.95, 0.9);
+	legend.SetHeader("#mu^{+}, 20 GeV");
+	legend.AddEntry(&hp20GeV, "Top panel", "l");
+	legend.AddEntry(&hs20GeV, "Side panel", "l");
+	legend.Draw();
+
+	TCanvas c2; c2.SetLogy();
+	gStyle->SetOptStat(110010);
+	TH1F hs20full("hs20full",";Energy deposition of vertically "
+			"down-going muons in side panel [MeV];",72,0,720);
+	tp20GeV.Draw("et[2]/1000>>hs20full");
+}
---
+++ rootlogon.C
@@ -0,0 +1,17 @@
+{ // https://root.cern.ch/doc/master/classTStyle.html
+	gStyle->SetLegendBorderSize(1);
+	gStyle->SetLegendFont(22);
+	gStyle->SetStatX(0.97);
+	gStyle->SetStatY(0.97);
+	gStyle->SetStatW(0.25);
+	gStyle->SetStatFontSize(0.05);
+	gStyle->SetStatFont(22);
+	gStyle->SetLabelFont(22,"XYZ");
+	gStyle->SetTitleFont(22,"XYZ");
+	gStyle->SetLabelSize(0.05,"XYZ");
+	gStyle->SetTitleSize(0.05,"XYZ");
+	gStyle->SetPadRightMargin(0.02);
+	gStyle->SetPadLeftMargin(0.08);
+	gStyle->SetPadTopMargin(0.02);
+	gStyle->SetPadBottomMargin(0.11);
+}
